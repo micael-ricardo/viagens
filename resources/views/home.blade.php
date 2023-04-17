ec44bdb39878d081993de5cd04c85f7d81e2e80d
micael-ricardo <micael.ricardo@outlook.com>
2023-04-17 15:02:11 -0300

    Inserir form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,117 @@
 @extends('layout')
 @section('title', 'Passagens')
 @section('conteudo')
-<h1> Teste </h1>
+
+
+    <div class="container">
+        <div class="card custom-bg w-75 p-4 d-flex">
+
+            <form id="flight-form" onsubmit="return validateForm()">
+                <div class="row">
+                    <div class="form-group col-md align-items-start flex-column">
+                        <label for="origin" class="d-inline-flex"> De </label>
+                        <input type="text" placeholder="Cidade ou Aeroporto" class="form-control" id="origin"
+                            name="origin" required>
+                    </div>
+                    <div class="form-group col-md align-items-start flex-column">
+                        <label for="partida" class="d-inline-flex"> Para </label>
+                        <input type="text" placeholder="Cidade ou Aeroporto" class="form-control" id="partida"
+                            name="partida" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-md align-items-start flex-column">
+                        <label for="data-partida" class=" d-inline-flex">Partida </label>
+                        <input type="date" class="form-control" id="data-partida" name="data-partida"
+                            onkeydown="return false" required>
+                    </div>
+                    <div class="form-group col-md align-items-start flex-column">
+                        <label for="data-retorno" class="d-inline-flex">Retorno</label>
+                        <input type="date" placeholder="One way" value=""
+                            onChange="this.setAttribute('value', this.value)" class="form-control" id="data-retorno"
+                            name="data-retorno">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-3 align-items-start flex-column">
+                        <label for="adultos" class="d-inline-flex col-auto"> Adultos <span class="sublabel"> 12+
+                            </span></label>
+                        <select class="form-select" id="adultos"
+                            onchange="javascript: dynamicDropDown(this.options[this.selectedIndex].value);">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-3 align-items-start flex-column">
+                        <label for="criancas" class="d-inline-flex col-auto">Crianças <span class="sublabel"> 2-11
+                            </span></label>
+                        <select class="form-select" id="criancas">
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-3 align-items-start flex-column">
+                        <label for="bebes" class="d-inline-flex col-auto">bebês <span class="sublabel">-2</span></label>
+                        <select class="form-select" id="bebes">
+                            <option value="0">0</option>
+                            <option value="1">1</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-6 align-items-start flex-column">
+                        <label for="cabine" class="d-inline-flex">Cabine</label>
+                        <select class="form-select" id="cabine">
+                            <option value="economica">Económica</option>
+                            <option value="economica_premium">Económica Premium </option>
+                            <option value="executiva">Executiva</option>
+                            <option value="Primeira">Primeira</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-6 align-items-start flex-column pt-lg-4">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input align-self-center" type="checkbox" id="directFlights">
+                            <label class="form-check-label d-inline-flex align-self-center" for="directFlights">Voos
+                                Direto</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-2">
+                    <div class="text-left col-auto">
+                        <button type="submit" class="btn btn-primary"> Pesquisar voos</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script>
+        function dynamicDropDown(listIndex) {
+
+            document.getElementById("bebes").length = 0;
+            document.getElementById("criancas").length = 0;
+
+            for (let i = 0; i < Number(listIndex) + 1; i++) {
+                document.getElementById("bebes").options[i] = new Option(i.toString(), i);
+            }
+
+            for (let i = 0; i < 9 - Number(listIndex) + 1; i++) {
+                document.getElementById("criancas").options[i] = new Option(i.toString(), i);
+            }
+        }
+    </script>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Passagens</title>
-  </head>
-  <body>
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    
+</head>
+
+<body style="background-image: url('/img/aviao.jpg');  background-size: cover;">
+    <nav class="navbar bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <i class="bi bi-airplane-engines"></i> Voos
+            </a>
+        </div>
+    </nav>
 
     @yield('conteudo')
-  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
